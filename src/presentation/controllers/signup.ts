4f7d02a6be1a26dd3d79943e0e5c8b1a246441aa
n0ks <rodrigocmp29@gmail.com>
2022-04-06 21:06:52 -0300

    feat(signup): test & implementation start

--- src/presentation/controllers/signup.ts
+++ src/presentation/controllers/signup.ts
@@ -1,5 +1,33 @@
+import { InvalidParamError, MissingParamsError } from '../errors';
+import { badRequest, serverError } from '../helpers/http-helper';
+import { EmailValidator } from '../protocols/email-validator';
+import { HttpRequest, HttpResponse } from '../protocols/http';
+
 export class SignUpController {
-  handle(request: any): any {
-    return { statusCode: 400 };
+  private readonly emailValidator: EmailValidator;
+
+  constructor(emailValidator: EmailValidator) {
+    this.emailValidator = emailValidator;
+  }
+
+  handle(request: HttpRequest): HttpResponse {
+    const requiredFields = ['name', 'email', 'password', 'passwordValidation'];
+
+    for (const field of requiredFields) {
+      if (!request.body[field]) {
+        return badRequest(new MissingParamsError(field));
+      }
+    }
+
+    try {
+      const isValid = this.emailValidator.isValid(request.body.email);
+      if (!isValid) {
+        return badRequest(new InvalidParamError('email'));
+      }
+    } catch (_) {
+      return serverError();
+    }
+
+    return { body: '', statusCode: 200 };
   }
 }
